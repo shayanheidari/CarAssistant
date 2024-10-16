0ad34c020a37eb61401041e530dcc43bd2569b79
shayanheidari <shaian.heidari@gmail.com>
2024-10-16 18:08:08 +0330

    initial

---
+++ makeDataset.py
@@ -0,0 +1,52 @@
+import sounddevice as sd
+from tqdm import tqdm
+from scipy.io.wavfile import write
+import os
+
+
+class MakeDataset():
+    def __init__(self, ds_path, n_sample):
+        self.ds_path = ds_path
+        self.n_sample = n_sample
+
+    def record_save_audio(self, class_name='sample'):
+        input(f'You are recording audio for class {class_name!r} '
+              f'press Enter to start: ')
+        for sample in tqdm(range(self.n_sample)):
+            fs = 44100
+            seconds = 2
+            if not os.path.exists(class_path := (self.ds_path + "/" + class_name)):
+                os.mkdir(class_path)
+            recorded_sound = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+            sd.wait()
+            save_path = os.path.join(self.ds_path, class_name, class_name)
+            write(save_path + '_' + str(sample) + '.wav', fs, recorded_sound)
+            input(f'{self.n_sample - sample} sample to record, press Enter to continue: ')
+            os.system('cls' if os.name == 'nt' else 'clear')
+
+    def record_save_background(self, class_name='background'):
+        input(f'You are recording audio for class {class_name!r} '
+              f'press Enter to start: ')
+        for sample in tqdm(range(self.n_sample)):
+            fs = 44100
+            seconds = 2
+            if not os.path.exists(class_path := (self.ds_path + "/" + class_name)):
+                os.mkdir(class_path)
+            recorded_sound = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+            sd.wait()
+            save_path = os.path.join(self.ds_path, class_name, class_name)
+            write(save_path + '_' + str(sample) + '.wav', fs, recorded_sound)
+            print(f'{self.n_sample - sample} sample to record, press Enter to continue: ')
+            os.system('cls' if os.name == 'nt' else 'clear')
+
+
+
+def main():
+    dataset_path = os.path.join('keyword')
+    keyword_ds = MakeDataset(ds_path=dataset_path, n_sample=3)
+    keyword_ds.record_save_audio(class_name='machine')
+    keyword_ds.record_save_background()
+
+
+if __name__ == '__main__':
+    main()
